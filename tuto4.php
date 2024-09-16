9d3c4ae2c0a4d0afa2b29a876becde7c5edb040d
abdelilahIbba <2001081800535@ofppt-edu.ma>
2024-09-16 18:01:56 +0100

    Polymorphism in PHP: Towards a more flexible code

---
+++ tuto4.php
@@ -0,0 +1,27 @@
+<?php
+class Animal {
+    public function makeSound() {
+        echo "The animal makes a sound.\n";
+    }
+}
+
+// Derived class: Dog
+class Dog extends Animal {
+    public function makeSound() {
+        echo "Woof!\n";
+    }
+}
+
+// Derived class: Cat
+class Cat extends Animal {
+    public function makeSound() {
+        echo "Meow!\n";
+    }
+}
+
+// Using polymorphism
+$animals = [new Dog(), new Cat(), new Animal()];
+
+foreach ($animals as $animal) {
+    $animal->makeSound();
+}
