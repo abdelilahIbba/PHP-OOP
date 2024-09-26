c325f1f64ff3891b4368034616c6fa4d32165d34
abdelilahIbba <2001081800535@ofppt-edu.ma>
2024-09-26 10:12:53 +0100

    protoType

---
+++ prototype/DataAccess/bookDAO.php
@@ -0,0 +1,14 @@
+<?php
+require "../DB/Database.php";
+class BookDAO {
+public function getBooks() {
+    $db = new Database();
+    return $db->Books;
+}
+public function setBooks($Book){
+$db = new Database();
+    $db->Books[] = $Book;
+    $db->saveData();
+}
+ 
+}
---
+++ prototype/Services/bookService.php
@@ -0,0 +1,15 @@
+<?php
+
+require "../DataAccess/BookDAO.php";
+
+class BookService {
+  public function getBooks() {
+      $bookDAO = new BookDAO();
+      return $bookDAO->getBooks();
+  }
+  public function setBooks($books){
+      $bookDAO = new BookDAO();
+      $bookDAO->setBooks($books);
+  }
+
+}
