f28af40572fc570eb2fe8a045165bb7097650965
abdelilahIbba <2001081800535@ofppt-edu.ma>
2024-09-27 12:56:52 +0100

    prototype (L.C)

--- Prototype/Service/BookService.php
+++ Prototype/Service/BookService.php
@@ -6,9 +6,18 @@ class Bookservices {
       $books = new BookDAO();
       return $books->getBooks();
   }
-    public function addbook($book)
+  public function addbook($book)
   {
     $bookDao = new BookDAO();
+    
+    $existingBooks = $bookDao->getBooks();
+    foreach ($existingBooks as $existingBook) {
+        if ($existingBook->getISBN() === $book->getISBN()) {
+            return "Book with ISBN: " . $book->getISBN() . " already exists!";
+        }
+    }
+
     $bookDao->AddBook($book);
+    return "Book added successfully.";
   }
 }
